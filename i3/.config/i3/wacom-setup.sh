672c7573e996ddd0c22eda9713769cbba347bd54
Neuro <neuro2718@gmail.com>
2026-05-04 20:46:34 +0530

    i3, kmonad, xinit

---
+++ i3/.config/i3/wacom-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Enable scrolling and make scrolling more sensitive than default
+xsetwacom --set "Wacom One by Wacom M Pen stylus" Button 2 "pan"
+xsetwacom --set "Wacom One by Wacom M Pen stylus" "PanScrollThreshold" 500
+
