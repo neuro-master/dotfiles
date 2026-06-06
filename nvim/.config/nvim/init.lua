97a1e5ff4907ff395920cde1c5ff95183509885d
Neuro <neuro2718@gmail.com>
2026-06-06 13:18:30 +0530

    initial neovim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- Print line numbers
+vim.o.number = true
+
+-- Use relative numbering for line numbers
+vim.o.relativenumber = true
+
+-- Use spaces instead of tabs (except Makefiles) by 'shiftwidth' tabs
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+
+-- Use smart autoindenting for C programs
+vim.o.smartindent = true
+
+
