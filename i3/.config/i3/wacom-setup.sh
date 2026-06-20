dcb5c8fe8077bf6d5c742d5c4743594e38856e1c
Neuro <neuro2718@gmail.com>
2026-06-20 18:29:41 +0530

    added note on wacom-setup for i3

--- i3/.config/i3/wacom-setup.sh
+++ i3/.config/i3/wacom-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# NOTE: ensure that 'xf86-input-wacom' package is installed
+
 # Enable scrolling and make scrolling more sensitive than default
 xsetwacom --set "Wacom One by Wacom M Pen stylus" Button 2 "pan"
 xsetwacom --set "Wacom One by Wacom M Pen stylus" "PanScrollThreshold" 500
